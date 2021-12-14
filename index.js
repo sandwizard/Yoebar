65d41bd119d9ad2cbfc0ef1b014b2b45b69b6936
sandwizard <10zinjts@gmail.com>
2021-12-14 19:10:30 +0530

    changes

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const http = require('http');
-const hostnamr = 'localhost';
+const hostname = 'localhost';
 const port = 3000;
+
+const fs = require('fs');
+const path = require('path');
+var indexhtml = "index.html"
+const server = http.createServer((req,res)=>{
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('content-type','text/html');
+    res.end('<html><body><h1>hrlo</h1></body></html>');
+
+})
+server.listen(port,hostname,()=>{
+    console.log(`server running at http://${hostname}:${port}`);
+});
--- js/home.js
+++
@@ -1,9 +0,0 @@
-
- (function (global) { 
-    var home = {} 
-    // loads when home snippet finishes//
-    const faders = document.querySelectorAll(".fade-in");
-    console.log("helo");
-    global.$yoebar.home = home;
- })(window)
- 
